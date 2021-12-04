62a536e1b76aeddf5f60ac5c905bb8d3d7f3a02a
chenleji <leji.chen@tenclass.com>
2021-12-04 18:25:47 +0800

    测试一下

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // 请求配置
 const config = {
-  api: 'http://bl.7yue.pro/v1/', //请求 url
-  appkey: 'Eyju2D0aL1QvmQpB' // 请求key
+  api: 'http://bl.talelin.com/v1/', //请求 url
+  appkey: '6pA5tk77OqdwGOdZ' // 请求key
 }
 
 export {
